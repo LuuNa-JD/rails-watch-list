bb9e8e77fd10d6a10d50d6c40420f0cfdce196ee
Julien Denizot <luunz71@gmail.com>
2023-10-24 21:34:25 -0400

    add upload image and uprage css design, v0.92

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_action :set_list, only: [:show]
+
   def index
     @lists = List.all
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
